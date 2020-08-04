92817581ffcd583a481d7c840700ff8e1271d023
Nguyen Tran Canh <nguyentrancanh81@gmail.com>
2020-08-04 20:45:30 +0700

    Fix Carousel

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 
 import { Router, Link } from "@reach/router";
 
 import SearchParams from "./SearchParams";
-
-import "./App.css";
 import Details from "./Details";
 
 function App() {
   return (
-    <React.StrictMode>
-      <div id="something-important">
-        <header>
-          <Link to="/">Adopt Me </Link>
-        </header>
-
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
-    </React.StrictMode>
+    <div>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
+    </div>
   );
 }
 
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,37 +1,39 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
 class Details extends React.Component {
-  constructor() {
-    super();
-    this.state = { loading: true };
-  }
+  state = { loading: true };
 
   componentDidMount() {
     pet
-      .animal(+this.props.id)
+      .animal(this.props.id)
       .then(({ animal }) => {
         this.setState({
           name: animal.name,
           animal: animal.type,
-          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          location: `${animal.contact.address.city}, ${
+            animal.contact.address.state
+            }`,
           description: animal.description,
           media: animal.photos,
           breed: animal.breeds.primary,
-          loading: false,
+          loading: false
         });
       })
-      .catch((err) => this.setState({ error: err }));
+      .catch(err => this.setState({ error: err }));
   }
   render() {
     if (this.state.loading) {
       return <h1>loading … </h1>;
     }
+    console.log(this.props.id)
 
     const { animal, breed, location, description, media, name } = this.state;
 
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${location}`}</h2>
