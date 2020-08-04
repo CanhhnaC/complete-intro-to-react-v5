c73f6a2464cf3cfb104831d88d3878f87eb06b70
Nguyen Tran Canh <nguyentrancanh81@gmail.com>
2020-08-04 15:11:55 +0700

    Async and routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
 import React from "react";
 
+import { Router, Link } from "@reach/router";
+
 import SearchParams from "./SearchParams";
 
 import "./App.css";
+import Details from "./Details";
 
 function App() {
   return (
-    <div id="something-important">
-      <h1>Adopt Me</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div id="something-important">
+        <header>
+          <Link to="/">Adopt Me </Link>
+        </header>
+
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
   );
 }
 
