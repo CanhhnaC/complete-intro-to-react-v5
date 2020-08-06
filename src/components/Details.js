11623e22da4b5afa7dd1c1fa717efb5bac7b9c95
Nguyen Tran Canh <nguyentrancanh81@gmail.com>
2020-08-06 09:53:05 +0700

    Add ErrorBoundary

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,7 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends React.Component {
   state = { loading: true };
@@ -12,37 +13,44 @@ class Details extends React.Component {
         this.setState({
           name: animal.name,
           animal: animal.type,
-          location: `${animal.contact.address.city}, ${
-            animal.contact.address.state
-            }`,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
           description: animal.description,
           media: animal.photos,
           breed: animal.breeds.primary,
-          loading: false
+          loading: false,
         });
       })
-      .catch(err => this.setState({ error: err }));
+      .catch((err) => this.setState({ error: err }));
   }
   render() {
     if (this.state.loading) {
       return <h1>loading … </h1>;
     }
-    console.log(this.props.id)
+    console.log(this.props.id);
 
     const { animal, breed, location, description, media, name } = this.state;
 
     return (
-      <div className="details">
-        <Carousel media={media} />
-        <div>
-          <h1>{name}</h1>
-          <h2>{`${animal} — ${breed} — ${location}`}</h2>
-          <button>Adopt {name}</button>
-          <p>{description}</p>
-        </div>
+        <div className="details">
+          <Carousel media={media} />
+          <div>
+            <h1>{name}</h1>
+            <h2>{`${animal} — ${breed} — ${location}`}</h2>
+            <button>Adopt {name}</button>
+            <p>{description}</p>
+          </div>
       </div>
     );
   }
 }
 
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+
+  return (
+    <ErrorBoundary>
+      <Details {...props}/>
+      
+    </ErrorBoundary>
+  )
+
+};
