02de45295aec4539129caf2fd0fee0a07dbca32f
Nguyen Tran Canh <nguyentrancanh81@gmail.com>
2020-08-07 17:36:26 +0700

    some thing

---
+++ src/components/Modal.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const modalRoot = document.getElementById("modal");
+
+const Modal = ({ children }) => {
+    const elRef = useRef(null)
+
+}
