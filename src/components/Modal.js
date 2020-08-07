b6fce2220aa2593865b45ee70f8409d0dd40d15c
Nguyen Tran Canh <nguyentrancanh81@gmail.com>
2020-08-07 18:28:52 +0700

    Fix something

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,23 +4,17 @@ import { createPortal } from "react-dom";
 const modalRoot = document.getElementById("modal");
 
 const Modal = ({ children }) => {
-<<<<<<< HEAD
-    const elRef = useRef(null)
+  const elRef = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
 
-}
-=======
-    const elRef = useRef(null);
-    if (!elRef.current) {
-        elRef.current = document.createElement("div");
-    }
+  useEffect(() => {
+    modalRoot.appendChild(elRef.current);
+    return () => modalRoot.removeChild(elRef.current);
+  }, []);
 
-    useEffect(() => {
-        modalRoot.appendChild(elRef.current);
-        return () => modalRoot.removeChild(elRef.current);
-    }, []);
-
-    return createPortal(<div>{children}</div>, elRef.current);
+  return createPortal(<div>{children}</div>, elRef.current);
 };
 
 export default Modal;
->>>>>>> 297a09f609ea238e8fadf760c114e28139417b36
